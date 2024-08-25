7f982a499002516b03b452e2b9780289473d422a
Vladyslav Ishchenko <vladyslavi@wix.com>
2024-08-25 20:13:03 +0300

    ios final project

---
+++ MovieFinder/Utils.swift
@@ -0,0 +1,21 @@
+//
+//  Utils.swift
+
+
+import Foundation
+
+class Utils {
+    
+    static let jsonDecoder: JSONDecoder = {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return jsonDecoder
+    }()
+    
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-mm-dd"
+        return dateFormatter
+    }()
+}
---
+++ MovieFinder/Views/UIKitViews/ActivityIndicatorView.swift
@@ -0,0 +1,16 @@
+//
+//  ActivityIndicatorView.swift
+
+
+import SwiftUI
+
+struct ActivityIndicatorView: UIViewRepresentable {
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {}
+
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let view = UIActivityIndicatorView(style: .large)
+        view.startAnimating()
+        return view
+    }
+}
